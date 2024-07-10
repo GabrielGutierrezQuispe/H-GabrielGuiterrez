7944b612927e60db92193ee184068a078d4acdfb
Gabriel Gutierrez Quispe <111787735+GabrielGutierrezQuispe@users.noreply.github.com>
2024-07-10 09:34:37 -0500

    initial project

---
+++ src/main/java/pe/edu/vallegrande/hgabrielgutierrez/HGabrielGutierrezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.hgabrielgutierrez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HGabrielGutierrezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HGabrielGutierrezApplication.class, args);
+    }
+
+}
